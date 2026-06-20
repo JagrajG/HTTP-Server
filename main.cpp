b28f4dd1b2ae0594ce69ec3dd96a87840fa2dbb3
JagrajG <jagrajgill83@gmail.com>
2026-06-20 14:19:46 -0700

    parsed request string to get the request method, path, and version

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <cstring>
 #include <sstream>
+#include <string>
 
 #define BUFFER_SIZE 4096
 #define PORT 8080
@@ -83,20 +84,6 @@ int main()
     char buffer[BUFFER_SIZE];
     int recieve_result = recv(client_fd, buffer, BUFFER_SIZE, 0);
 
-    std::string start_line(buffer, recieve_result);
-    std::string target = "\r\n";
-    size_t pos_target = start_line.find(target);
-
-    if (pos_target == std::string::npos)
-    {
-        std::cout << "Could not find end of request line\n";
-    }
-
-    else
-    {
-        std::string request_line = start_line.substr(0, pos_target);
-    }
-
     if (recieve_result > 0)
     {
         std::cout << "Read: " << recieve_result << " Byte\n";
@@ -109,6 +96,30 @@ int main()
     else
     {
         std::cout << "Data recieve fail\n";
+    }
+
+    std::string start_line(buffer, recieve_result);
+    std::string target = "\r\n";
+    size_t pos_target = start_line.find(target);
+
+    if (pos_target == std::string::npos)
+    {
+        std::cout << "Could not find end of request line\n";
+    }
+
+    else
+    {
+        std::string request_line = start_line.substr(0, pos_target);
+
+        HttpRequest request;
+        std::istringstream iss(request_line);
+
+        if (iss >> request.method >> request.path >> request.version)
+        {
+            std::cout << "Method: " << request.method << "\n";
+            std::cout << "Path: " << request.path << "\n";
+            std::cout << "Version: " << request.version << "\n";
+        }
     }
 
     std::string body = "<h1>Hello from my C++ server</h1>";
@@ -131,6 +142,7 @@ int main()
         std::cout << "Data sent sucesfully\n";
     }
 
+    close(client_fd);
     close(sockfd);
     return 0;
 }
